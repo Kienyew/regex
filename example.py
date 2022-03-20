4ce734e15349e609b228454bcf21f171672aeceb
Kienyew <aobi100bt@hotmail.com>
2022-03-20 20:14:38 +0800

    Add example.py

---
+++ example.py
@@ -0,0 +1,17 @@
+import regex
+
+a = regex.NFA.char('a')  # construct NFA of regex 'a'
+b = regex.NFA.char('b')  # construct NFA of regex 'b'
+union = a.union(b)  # construct NFA of regex 'a|b'
+closure = union.closure()  # construct NFA or regex '(a|b)*'
+
+nfa = closure
+print(nfa.to_graphviz())
+
+dfa = regex.subset_construction(nfa)
+print(dfa.to_graphviz())
+
+min_dfa = regex.dfa_minimize(dfa)
+print(min_dfa.to_graphviz())
+
+print(regex.parse('(a|b)*').to_graphviz())
